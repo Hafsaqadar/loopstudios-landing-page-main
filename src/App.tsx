44237904c9dbd6c38a9c2ae7e756e14911ef53b4
hafsaqadar <haf252537@gmail.com>
2025-07-28 21:02:24 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Products from "./components/Products";
 
 export default function App() {
   return (
    <div>
      <Navbar />
+     <Hero />
+     <Products />
    </div>
   )
 }
